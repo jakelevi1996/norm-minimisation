3a698f6aab5e63975f91d7f1b3535af601445659
jakelevi1996 <jakelevi@hotmail.co.uk>
2018-11-23 23:06:32 +0000

    Convert .csv to .npz and load as `np.ndarray`s

---
+++ Code/Data/fileio.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+DEFAULT_FOLDER = "Code/data/"
+DEFAULT_FILENAME = DEFAULT_FOLDER + "matrices.npz"
+
+a_list = ["A" + str(i) for i in range(1, 6)]
+b_list = ["b" + str(i) for i in range(1, 6)]
+DEFAULT_FILES_LIST = a_list + b_list
+
+def csv_to_npz(
+    input_folder=DEFAULT_FOLDER,
+    input_files=DEFAULT_FILES_LIST,
+    output_filename=DEFAULT_FILENAME
+):
+    array_list = [np.loadtxt(
+        input_folder + file + ".csv", delimiter=','
+    ) for file in input_files]
+
+    np.savez(output_filename, **{
+        file: array_list[i] for i, file in enumerate(input_files)
+    })
+
+def load_A_b(index=1, filename=DEFAULT_FILENAME):
+    with np.load(filename) as data:
+        # array_list = [data[name] for name in array_names]
+        A = data["A" + str(index)]
+        b = data["b" + str(index)]
+    return A, b
+
+if __name__ == "__main__":
+    print(DEFAULT_FILES_LIST)
+    # csv_to_npz()
+    A, b = load_A_b()
+    print(A.shape)
+    print(b.shape)
+    # print(A)
+    # print(b)
--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 # practical-optimisation
 
 This repository is primarily for coursework submissions to 4M17: 'Practical optimisation'
+
+## File structure description
+
+*Coming soon.*
+
+## Usage description
+
+*Coming soon.*
+
+## TODO
+
+*Coming soon.*
